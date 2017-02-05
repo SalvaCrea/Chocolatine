3498bc90663f8c5351aa72dda0d59b8ba2818aaf
Cardona <hebustha@gmail.com>
2017-02-05 18:00:16 +0100

    Uglu 0.3

--- salva-powa.php
+++ salva-powa.php
@@ -9,6 +9,18 @@
  * License: GPL2
  */
 
+ function pippin_taxonomy_add_new_meta_field() {
+ 	// this will add the custom meta field to the add new term page
+ 	?>
+ 	<div class="form-field">
+ 		<label for="term_meta[custom_term_meta]"><?php _e( 'Example meta field', 'pippin' ); ?></label>
+ 		<input type="text" name="term_meta[custom_term_meta]" id="term_meta[custom_term_meta]" value="">
+ 		<p class="description"><?php _e( 'Enter a value for this field','pippin' ); ?></p>
+ 	</div>
+ <?php
+ }
+ add_action( 'category_add_form_fields', 'pippin_taxonomy_add_new_meta_field', 10, 2 );
+ 
 // The awesome global who content this informations not importants
 $sp = array(
   'url' => plugin_dir_url( __FILE__ )
