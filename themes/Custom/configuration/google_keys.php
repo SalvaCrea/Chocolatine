c58d4c6650546445dfa5e592c06d75587274e8c1
Cardona <hebustha@gmail.com>
2017-10-27 19:10:35 +0200

    ++

--- themes/Custom/configuration/google_keys.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'id_client' => '286279837453-hgp4rs0aj2ml2i39dhjj79sbiich79ig.apps.googleusercontent.com',
-    'secret_key' => '_E2o3tbUXn7DTHC55btB74yL',
-    'application_name' => 'Data Analytic For ClearBlue',
-    'developer_key' => 'AIzaSyBpIgbLaz_3CMfAyPhqyAPKmAvkF-UscJk',
-    'redirect_uri' => '/google_api_redirect'
-];
